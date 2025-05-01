51fe4f99e092dcfa1f4eed8daabfd8e7f4ec4abb
Jessie @1nAp <89487877+1nAp@users.noreply.github.com>
2025-05-01 23:05:17 +0200

    Add files via upload

---
+++ Nato Alphabet Encoder/main.py
@@ -0,0 +1,42 @@
+student_dict = {
+    "student": ["Angela", "James", "Lily"], 
+    "score": [56, 76, 98]
+}
+
+#Looping through dictionaries:
+for (key, value) in student_dict.items():
+    #Access key and value
+    pass
+
+import pandas
+student_data_frame = pandas.DataFrame(student_dict)
+
+#Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     print(row)
+    #Access index and row
+    #Access row.student or row.score
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+#{"A": "Alfa", "B": "Bravo"}
+
+nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+print("Nato_dictionary:")
+print(nato_dict)
+
+#Creates a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    user_word = input("Enter a word: ").upper()
+    try:
+        phonetic_code_word = [nato_dict[letter] for letter in user_word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(phonetic_code_word)
+
+
+generate_phonetic()
